fcfdff1a59beab6fd59c61c0f80f1d7c6683f198
Pat McGhen <patrickmcghen@gmail.com>
2022-06-02 22:20:49 -0400

    Initial commit.

---
+++ readme.md
@@ -0,0 +1,8 @@
+# 👾 Alien Invasion! 🚀
+
+Building a Space Invaders clone in Python.
+
+## Acknowledgements
+
+[Python Crash Course, 2nd Edition](https://nostarch.com/pythoncrashcourse2e)
+[Spaceship sprite](https://pixabay.com/illustrations/spaceship-alien-paint-colorful-6061489/)
---
+++ settings.py
@@ -0,0 +1,10 @@
+class Settings:
+    """All the settings for Alien Invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        self.SCREEN_WIDTH = 1024
+        self.SCREEN_HEIGHT = 768
+        self.BG_COLOR = (33,26,33)
+
+        self.ship_speed = 1.5
